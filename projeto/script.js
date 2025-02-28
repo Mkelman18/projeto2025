a2cb299a3f5cd206b7635b5f3544a2403e7c6f5a
Mkelman18 <160253161+Mkelman18@users.noreply.github.com>
2025-02-28 10:58:04 -0300

    Add files via upload

---
+++ projeto/script.js
@@ -0,0 +1,10 @@
+var nome = prompt("Digite seu Nome");
+var email = prompt("Digite seu E-mail");
+var numero_cartao = prompt("Digite seu Número de Cartão de Crédito");
+var senha = prompt("Digite sua Senha");
+
+
+console.log(nome);
+console.log(email);
+console.log(numero_cartao);
+console.log(senha);
